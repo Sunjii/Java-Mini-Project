b582404bac46beacf39d2d0807bbb44095e95fea
Sunji <sophieadenn@gmail.com>
2019-11-17 21:32:25 +0900

    상수추가

--- Pol/src/Constant.java
+++ Pol/src/Constant.java
@@ -1,11 +1,14 @@
 // 상수 정의 클래스
 public class Constant {
-	
+	// 오염물질
 	static final String pollut_0 = "미세먼지";
 	static final String pollut_1 = "초미세먼지";
 	static final String pollut_2 = "오존";
 	static final String pollut_3 = "";
 	
+	// 대화상자 사이즈
+	static final int dial_W = 600;
+	static final int dial_H = 350;
 	
 
 }
