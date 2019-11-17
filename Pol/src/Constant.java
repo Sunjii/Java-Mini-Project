18d1ea33f941de0b9e1b7bed0c3ada8738ce9889
Sunji <sophieadenn@gmail.com>
2019-11-17 12:32:22 +0900

    상수 클래스 생성했음

---
+++ Pol/src/Constant.java
@@ -0,0 +1,11 @@
+// 상수 정의 클래스
+public class Constant {
+	
+	static final String pollut_0 = "미세먼지";
+	static final String pollut_1 = "초미세먼지";
+	static final String pollut_2 = "오존";
+	static final String pollut_3 = "";
+	
+	
+
+}
