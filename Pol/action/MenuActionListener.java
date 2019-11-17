d566e42d740cb3592acede7e6458133fdaf3640b
Sunji <sophieadenn@gmail.com>
2019-11-17 22:19:22 +0900

    테이블에서 선택된 데이터로 그래프 그리는 기능 추가

--- Pol/action/MenuActionListener.java
+++ Pol/action/MenuActionListener.java
@@ -26,8 +26,34 @@ public class MenuActionListener implements ActionListener{
 			JOptionPane.showMessageDialog(null, "아직 DB기능은 미구현입니다.", "데이터 없음", JOptionPane.ERROR_MESSAGE);
 			break;
 		case "지역별 조회":
-			Frame.gDialog.setVisible(true);
-			break;
+			// 테이블에서 어딘가 선택되었을때만 그려지도록 한다.
+			switch(Frame.resTable.getSelectedRowCount()) {
+			case 0:
+				JOptionPane.showMessageDialog(null, "선택된 칼럼이 없습니다!");
+				break;
+			case 1:
+				// 선택한 칼럼에서 데이터 가져오기
+				int row = Frame.resTable.getSelectedRow();
+				int col = Frame.resTable.getSelectedColumn();
+				
+				String area = (String) Frame.resTable.getValueAt(row, 0);
+				int pol1 = Integer.valueOf((String) (Frame.resTable.getValueAt(row, 2)));
+				int pol2 = Integer.valueOf((String) (Frame.resTable.getValueAt(row, 3)));
+				int pol3 = Integer.valueOf((String) (Frame.resTable.getValueAt(row, 4)));
+
+				System.out.println(row + ", " + col);
+				System.out.println(area + " " + pol1);
+				// 데이터 적용 시키기
+				Frame.cgDialog.setNumbers(pol1, pol2, pol3);
+				// 데이터를 기반으로 그래프 그리기
+				Frame.cgDialog.setVisible(true);
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "원형 그래프는 하나의 칼럼만 그릴 수 있습니다.");
+				break;
+					
+			}
+			
 		case "기간별 조회":
 			break;
 		
