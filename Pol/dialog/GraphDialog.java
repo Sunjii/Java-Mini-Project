0dc52ec5040e6480b7d2865ddd24d494e801ac9f
Sunji <sophieadenn@gmail.com>
2019-11-17 21:31:47 +0900

    코드정리

--- Pol/dialog/GraphDialog.java
+++ Pol/dialog/GraphDialog.java
@@ -1,22 +1,38 @@
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GraphDialog extends JDialog{
 
 	JButton closeBtn = new JButton("닫기");
+	JButton paintBtn = new JButton("그리기");
+	
+	CircleGraph graph = new CircleGraph();
 	
 	public GraphDialog(JFrame jframe, String title) {
 		super(jframe, title);
-		setLayout(new FlowLayout());
-		add(new JLabel("TEST"));
+		setLayout(new BorderLayout());
 		
-		setSize(300,300);
+		// 버튼 패널
+		JPanel btnP = new JPanel();
+		btnP.add(paintBtn);
+		btnP.add(closeBtn);
+		add(btnP, BorderLayout.SOUTH);
+		add(graph, BorderLayout.CENTER);
 		
-		closeBtn.addActionListener(e -> setVisible(false)); // 닫기버튼 리스너추가
+		setSize(Constant.dial_W, Constant.dial_H);
+		
+		// 그리기 버튼 리스너
+		paintBtn.addActionListener(e -> {
+			graph.setNumbers(10, 20, 30);
+			graph.repaint();
+		});
+		
+		// 닫기 버튼 리스너
+		closeBtn.addActionListener(e -> setVisible(false));
 		
 	}
 	
