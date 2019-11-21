e05611e1ba73ca77252fbbbcac9c3579e87a1f94
Sunji <sophieadenn@gmail.com>
2019-11-22 02:37:41 +0900

    접근제한자 수정, 상수화

--- Pol/graphics/BarGraph.java
+++ Pol/graphics/BarGraph.java
@@ -4,16 +4,19 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class BarGraph extends JPanel{
+	private double num[] = {0, 0, 0, 0, 0, 0}; // 오염물질의 농도
+	private double max[] = {0.060, 0.060, 3, 0.050, 200, 130}; // 오염물질들의 최대값
 	
-	int gap;
-
-	double num[] = {0, 0, 0, 0, 0, 0}; // 오염물질의 농도
-	double max[] = {0.060, 0.060, 3, 0.050, 200, 130}; // 오염물질들의 최대값
+	private static final int bar_w = 10; // 막대그래프 폭
+	private int bar_h; // 막대그래프 높이
 	
-	int bar_w = 10; // 막대그래프 폭
-	int bar_h; // 막대그래프 높이
+	private static final int graph_pos_x = 40; // 그래프 틀 시작좌표
+	private static final int graph_pos_y = 40;
+	private static final int graph_w = 420;	// 그래프 폭
+	private static final int graph_h = 280;	// 그래프 높이
 	
-	int s_x, s_y; // 막대 시작점 좌표
+	private int s_x, s_y; // 막대 시작점 좌표
+	private int gap;
 
 	public void paint(Graphics g) {
 		g.clearRect(0, 0, getWidth(), getHeight());
@@ -22,33 +25,30 @@ public class BarGraph extends JPanel{
 		
 		// 그래프 틀
 		g.setColor(Color.black);
-		g.drawRect(40, 40, 420, 280);
-		gap = 420 / 6; // 간격
+		g.drawRect(graph_pos_x, graph_pos_y, graph_w, graph_h);
+		gap = graph_w / 6; // 간격
 		// x축
 		for(int i=0; i<6; i++) {
-			g.drawLine(40+gap, 40+280, 40+gap, 40);
-			g.drawString(Constant.pollut[i], (gap)-20, 40+280+15);
+			g.drawLine(graph_pos_x+gap, graph_pos_y+graph_h, graph_pos_x+gap, graph_pos_y);
+			g.drawString(Constant.pollut[i], (gap)-20, graph_pos_y + graph_h + 15);
 			gap += 420/6;
 		}
 		// y축
 		for(int i=0; i<10; i++) {
-			g.drawLine(40, 40+(i*28), 40+420, 40+(i*28));
-			g.drawString(Integer.toString(100 - i*10), 40-22, 40+(i*28));
+			g.drawLine(graph_pos_x, graph_pos_y+(i*28), graph_pos_x+graph_w, graph_pos_y+(i*28));
+			g.drawString(Integer.toString(100 - i*10), graph_pos_x-22, graph_pos_y+(i*28));
 		}
 		
 		// num1 ~ num6의 막대 그래프 그리기
 		// bar_h : 상대값. 
 		// s_y : 40 ~ 320
 		gap = 420/6;
-		for (int i=0; i<6; i++) {
-			System.out.println(num[i]);
-		}
 		
 		for(int i=0; i<6; i++) {
-			s_x = 40 + gap/2;
+			s_x = graph_pos_x + gap/2;
 			bar_h = (int) (num[i] / max[i] * 100);
-			s_y = 320 - bar_h;
-			gap += 420/3;
+			s_y = graph_pos_y + graph_h - bar_h;
+			gap += graph_w/3;
 			g.setColor(Color.red);
 			g.fillRect(s_x, s_y, bar_w, bar_h);		
 		}
