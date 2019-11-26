a93397b7dedef9172dc4433c13c3d63dca08fcba
Damas J Lee <sophieadenn@gmail.com>
2019-11-26 10:36:54 +0900

    Stat의 자료형을 모두 double로 바꿨음

--- Pol/src/Stat.java
+++ Pol/src/Stat.java
@@ -4,10 +4,13 @@ public class Stat {
 	protected double oppm;		//오존농도
 	protected double cppm;		//이산화탄소
 	protected double appm;		//아황산가스
-	protected int dust;		//미세먼지
-	protected int mdust;		//초미세먼지
+	//protected int dust;		//미세먼지
+	//protected int mdust;		//초미세먼지
+	protected double dust;
+	protected double mdust;
 	
-	public Stat(double nppm, double oppm, double cppm, double appm, int dust, int mdust) {
+	
+	public Stat(double nppm, double oppm, double cppm, double appm, double dust, double mdust) {
 		this.nppm = nppm;
 		this.oppm = oppm;
 		this.cppm = cppm;
