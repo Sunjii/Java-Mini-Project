986aafee993f2b0b9e6250a8b44bf5f0263b5aae
Sunji <sophieadenn@gmail.com>
2019-11-30 22:16:49 +0900

    막대그래프 완전구현

--- Pol/graphics/BarGraph.java
+++ Pol/graphics/BarGraph.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Window;
 import java.lang.management.GarbageCollectorMXBean;
 import java.time.LocalDate;
 
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -25,14 +28,18 @@ public class BarGraph extends JPanel{
 	
 	private LocalDate date; // 그릴 날짜
 	private String item; // 그릴 오염물질
+	private int type; // 타입
 	
-	
-	int plus = 1600;  // 막대그래프에서 추가로 늘어나는 폭
+	private int plus = 1600;  // 막대그래프에서 추가로 늘어나는 폭
+	private int plus2 = 400;
 
+	
 	public void paint(Graphics g) {
 		g.clearRect(0, 0, getWidth(), getHeight());
 		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(0, 0, Constant.dial_W + plus, Constant.dial_H);
+		g.fillRect(0, 0, Constant.dial_W + plus, Constant.dial_H + plus2);
+		
+		setType(item);
 		
 		gap = (graph_w+plus) / (Constant.locations.length -1); // 간격. 46개지역
 		// 그래프 틀
@@ -44,10 +51,16 @@ public class BarGraph extends JPanel{
 			//gap += 420/6;
 			gap += (graph_w + plus) / (Constant.locations.length -1);
 		}
+		
+		
 		// y축
 		for(int i=0; i<10; i++) {
+			//g.drawLine(graph_pos_x, graph_pos_y+(i*28), graph_pos_x+graph_w+plus, graph_pos_y+(i*28));
 			g.drawLine(graph_pos_x, graph_pos_y+(i*28), graph_pos_x+graph_w+plus, graph_pos_y+(i*28));
-			g.drawString(Integer.toString(100 - i*10), graph_pos_x-22, graph_pos_y+(i*28));
+			//g.drawString(Integer.toString(100 - i*10), graph_pos_x-22, graph_pos_y+(i*28));
+			double dou = Constant.pollut_max[type] - i *(Constant.pollut_max[type]/10);
+			String str = String.format("%.3f", dou);
+			g.drawString(str, graph_pos_x-22, graph_pos_y+(i*28));
 		}
 		
 
@@ -70,9 +83,65 @@ public class BarGraph extends JPanel{
 			g.setColor(Color.red);
 			g.fillRect(s_x, s_y, bar_w, bar_h);
 		}
+		g.setFont(new Font("맑은 고딕", Font.BOLD, 24));
+		g.setColor(Color.BLACK);
+		g.drawString(date.toString(), 50, graph_pos_y + graph_h + 100);
+		g.setFont(new Font("맑은 고딕", Font.PLAIN, 18));
+		g.drawString(item + "의 농도", 50, graph_pos_y + graph_h + 130);
+		
+		//printStn(item);
+		
+		switch (item) {
+		case "이산화질소":
+			g.drawString(Constant.nppmS, 50, graph_pos_y + graph_h + 180);
+			break;
+		case "오존농도":
+			g.drawString(Constant.oppmS, 50, graph_pos_y + graph_h + 180);
+			break;
+		case "이산화탄소":
+			g.drawString(Constant.cppmS, 50, graph_pos_y + graph_h + 180);
+			break;
+		case "아황산가스":
+			g.drawString(Constant.appmS, 50, graph_pos_y + graph_h + 180);
+			break;
+		case "미세먼지":
+			g.drawString(Constant.dustS, 50, graph_pos_y + graph_h + 180);
+			break;
+		case "초미세먼지":
+			g.drawString(Constant.mdustS, 50, graph_pos_y + graph_h + 180);
+			break;
+		}
+		
+		
 
 	}
 	
+
+	private void setType(String item) {
+		switch (item) {
+		case "이산화질소":
+			this.type = 0;
+			break;
+		case "오존농도":
+			this.type = 1;
+			break;
+		case "이산화탄소":
+			this.type = 2;
+			break;
+		case "아황산가스":
+			this.type = 3;
+			break;
+		case "미세먼지":
+			this.type = 4;
+			break;
+		case "초미세먼지":
+			this.type = 5;
+			break;
+		}
+		
+	}
+
+
 	private int getBar(int i, LocalDate date, String item) {
 		// i번째 도시 (1부터 시작) 의 date날의 item에 해당하는 오염물질의 농도를 가져오는 함수
 
@@ -93,27 +162,23 @@ public class BarGraph extends JPanel{
 			return -1; // 값 없음
 		}
 		
+		// 1칸 28px, max/10
+		// 10칸이 max
+		
+		
 		// 찾아낸 ppm을 상대값화 시킨다. 각 오염물질의 값 범위는 max를 참조한다.
 		for (int k=0; k<Constant.pollut.length; k++) {
 			if(item.equals(Constant.pollut[k])) {
-				return (int)(ppm / Constant.pollut_max[k] * 100);
+				//System.out.println((ppm / Constant.pollut_max[k] * 100));
+				//return (int)(ppm / Constant.pollut_max[k] * 100);
+				double result = (ppm / Constant.pollut_max[k] * 100); // ex) 21  == 21%.. 즉 2.1칸		
+				return (int)(result / 10 * 28);
 			}
 		}
 		
 		return -1;
 		
 	}
-
-	/*
-	public void setNumbers(double num1, double num2, double num3, double num4, double num5, double num6) {
-		num[0] = num1;
-		num[1] = num2;
-		num[2] = num3;
-		num[3] = num4;
-		num[4] = num5;
-		num[5] = num6;
-	}
-	*/
 
 	public void setDate(LocalDate selectedLocalDate) {
 		this.date = selectedLocalDate;
