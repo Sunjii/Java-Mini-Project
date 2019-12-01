088d8869eea5a69c031b9f9b1e2b6588e99805de
Sunji <sophieadenn@gmail.com>
2019-12-01 19:40:23 +0900

    선 그리기 까지 구현됨

--- Pol/graphics/LineGraph.java
+++ Pol/graphics/LineGraph.java
@@ -1,5 +1,7 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
@@ -31,6 +33,8 @@ public class LineGraph extends JPanel {
 	
 	private ArrayList<String> dateList = new ArrayList<String>();
 	
+	//private int xArr[];
+	//private int yArr[];
 	
 	// 일정 기간 동안 한 물질의 수치를 꺽은선 그래프로 표현
 	public void paint(Graphics g) {
@@ -67,7 +71,7 @@ public class LineGraph extends JPanel {
 		g.setColor(Color.black);
 		// x축
 		gap = graph_w / dateLength;
-		
+		g.drawLine(graph_pos_x, graph_pos_y+graph_h, graph_pos_x, graph_pos_y);
 		for (int i=0; i < dateLength; i++) {
 			g.drawLine(graph_pos_x+gap, graph_pos_y+graph_h, graph_pos_x+gap, graph_pos_y);	// x 축에 수직인 선
 			String d = dateList.get(i).split("-")[1] + "-" + dateList.get(i).split("-")[2];
@@ -88,15 +92,67 @@ public class LineGraph extends JPanel {
 		// 그래프 그리기
 		
 		// 각 날짜마다의 y값을 계산
+		gap = graph_w / dateLength;
+		gap -= 20;
+		int [] xArr = new int [dateLength];
+		int [] yArr = new int [dateLength];
 		
-		// drawPolyline(int[] x, int[] y, numberofLine);
+		// 선의 꼭짓점을 배열에 넣는다.
+		for (int i=0; i<dateLength; i++) {
+			xArr[i] = graph_pos_x+gap;
+			//yArr[i] = 150 + (i*10);
+			int buf = getPoint(i, dateList, item);
+			if (buf < 0) {
+				buf = 0;
+			}
+			buf = graph_pos_y + graph_h - buf;
+			yArr[i] = buf;
+			gap += graph_w / dateLength;
+		}
 		
+		// 선을 그린다.
+		g.setColor(Color.red);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setStroke(new BasicStroke(5)); // 선 굵기 설정
+		g.drawPolyline(xArr, yArr, dateLength);
 		
+	}
+
+
+	// i번째 날의 item 농도를 계산하여 반환.
+	private int getPoint(int i, ArrayList<String> dateList, String item) {
 		
+		// dateS 와 i 를 이용하여 찾으려는 날짜를 만든다.
+		LocalDate curD = LocalDate.parse(dateList.get(i));
 		
+		// location에서 curD의 item 농도를 탐색.
+		Stat resStat = new Stat(0,0,0,0,0,0);
+		if(Frame.csvL.findLocation(this.location, curD) != null) {
+			resStat = Frame.csvL.findLocation(this.location, curD).getStat();
+		} else {
+			return -1; // 데이터 없음
+		}
 		
+		// resStat 에서 원하는 item의 농도를 탐색.
+		double ppm = -1;
+		ppm = resStat.getPpm(item);
+		if (ppm == -1) {
+			return -1; // 데이터 없음
+		}
 		
-		
+		// 10칸이 max, 1칸 당 48px
+		// 찾아낸 ppm을 상대값화 시킨다. 각 오염물질의 값 범위는 max를 참조한다.
+		for (int k=0; k<Constant.pollut.length; k++) {
+			if(item.equals(Constant.pollut[k])) {
+				//double result = (ppm / Constant.pollut_max[k] * 100); // ex) 21  == 21%.. 즉 2.1칸		
+				//return (int)(result / 10 * 48);
+				double result = (ppm / Constant.pollut_max[k] * 100);
+				//System.out.println(i + "날 " + location + "의 " + item + " " + ppm);
+				return (int)(result / 10 * 48);
+				
+			}
+		}
+		return -1;
 	}
 
 
