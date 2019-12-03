ea52d7e301a860aecfbb926622e3d0a9b2c626a2
Sunji <sophieadenn@gmail.com>
2019-12-04 02:57:31 +0900

    지도 기능 구현중... 그라데이션 강도 조절해야함

--- Pol/graphics/Map.java
+++ Pol/graphics/Map.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
+import java.time.LocalDate;
 
 import javax.swing.JPanel;
 
@@ -33,7 +35,8 @@ public class Map extends JPanel {
 	private int s6_num = 10;
 */
 	
-	String item;
+	private String item = "미세먼지";			// 오염물질
+	private LocalDate date = LocalDate.parse("2018-01-01");	// 날짜
 					
 
 	public void paint(Graphics g) {
@@ -49,12 +52,12 @@ public class Map extends JPanel {
 		g.drawPolygon(Constant.s5_xp, Constant.s5_yp, Constant.s5_num);
 		g.drawPolygon(Constant.s6_xp, Constant.s6_yp, Constant.s6_num);
 		
-		
-		//makeFill(g);
-		
-		
+		System.out.println(date.toString() + " 날짜, 아이템 " + item);
+		makeFill(g);
 		
 		
+		
+	
 		
 		
 		
@@ -69,25 +72,29 @@ public class Map extends JPanel {
 		
 		for (int i=0; i<6; i++) {
 			// i sector의 그라데이션 세기를 구한다.
-			power[i] = getColor(i, item);			
+			power[i] = getColor(i, item);
+			System.out.println(power[i]);
 		}
 		
-		g.setColor(new Color(power[0], 10, 10));
+		
+		
+		
+		g.setColor(new Color(200, power[0], 10));
 		g.fillPolygon(Constant.s1_xp, Constant.s1_yp, Constant.s1_num);
 		
-		g.setColor(new Color(power[1], 10, 10));
+		g.setColor(new Color(200, power[1], 10));
 		g.fillPolygon(Constant.s2_xp, Constant.s2_yp, Constant.s2_num);
 		
-		g.setColor(new Color(power[2], 10, 10));
+		g.setColor(new Color(200, power[2], 10));
 		g.fillPolygon(Constant.s3_xp, Constant.s3_yp, Constant.s3_num);
 		
-		g.setColor(new Color(power[3], 10, 10));
+		g.setColor(new Color(200, power[3], 10));
 		g.fillPolygon(Constant.s4_xp, Constant.s4_yp, Constant.s4_num);
 		
-		g.setColor(new Color(power[4], 10, 10));
+		g.setColor(new Color(200, power[4], 10));
 		g.fillPolygon(Constant.s5_xp, Constant.s5_yp, Constant.s5_num);
 		
-		g.setColor(new Color(power[5], 10, 10));
+		g.setColor(new Color(200, power[5], 10));
 		g.fillPolygon(Constant.s6_xp, Constant.s6_yp, Constant.s6_num);
 		
 		
@@ -96,13 +103,88 @@ public class Map extends JPanel {
 		
 	}
 
-	private int getColor(int i, String item) {
-		// TODO Auto-generated method stu
+	private int getColor(int s, String item) {
+		double sum = 0;
+		double count = 0;
+		// 현재 섹터는 s 번 섹터.
+		// 현재 날짜는 date, 찾으려는 물질은 item
+		// date의 item 농도를 가져온다.		
+		/*
+		for(int i=0; i<Constant.locations.length; i++) {
+			Stat resStat = new Stat(0,0,0,0,0,0);
+			if(Frame.csvL.findLocation(Constant.locations[i], date) != null) {
+				resStat = Frame.csvL.findLocation(Constant.locations[i], date).getStat();
+				
+				double ppm = 0;
+				ppm = resStat.getPpm(item);	// item의 농도를 가져옴
+				if(ppm == -1) {
+					// 값이 없는 경우임.
+					ppm = 0;
+				} else {
+					count++;
+				}
+				sum += ppm;
+			}
+		}
+		*/
+		for(int i=0; i < Constant.sectorList.get(s).length; i++) {
+			Stat resStat = new Stat(0,0,0,0,0,0);
+			if(Frame.csvL.findLocation(Constant.sectorList.get(s)[i], date) != null) {
+				resStat = Frame.csvL.findLocation(Constant.sectorList.get(s)[i], date).getStat();
+				
+				double ppm = 0;
+				ppm = resStat.getPpm(item);	// item의 농도를 가져옴
+				if(ppm == -1) {
+					// 값이 없는 경우임.
+					ppm = 0;
+				} else {
+					count++;
+				}
+				sum += ppm;
+			}
+		}
+		// 전체 지역에서 item의 평균을 구함.
+		double avg = 0;
+		avg = sum/count;
+		
+		
+		System.out.println(sum + " = sum, " + avg);
+		int result = 0; // 0~255
+		// 각 오염물질의 권고기준을 찾아낸다.
+		switch (item) {
+		case "이산화질소":
+			result = (int) ((avg/0.3) * 100) ;
+			break;
+		case "오존농도":
+			result = (int) ((avg/0.2) * 100) ;		
+			break;
+		case "이산화탄소":
+			result = (int) ((avg/20) * 100) ;		
+			break;
+		case "아황산가스":
+			result = (int) ((avg/0.2) * 100) ;			
+			break;
+		case "미세먼지":
+			result = (int) ((avg/200) * 100) ;			
+			break;			
+		case "초미세먼지":
+			result = (int) ((avg/100) * 100) ;
+			break;
+		}
 		
 		
 		
+		return 255-result;
+	}
+
+	public void setDate(String searchDate) {
+		this.date = LocalDate.parse(searchDate);
 		
-		return 0;
+	}
+
+	public void setItem(String string) {
+		this.item = string;
+		
 	}
 	
 	
